c5c7a2c87f6c5094a1a801d9d8b515876f66b637
Xinya Kuang <kuangxinyaya@gmail.com>
2023-03-17 15:32:32 +0100

    qgis ang openlayer

---
+++ app/src/JQgisAOpenlayer.js
@@ -0,0 +1,88 @@
+import GeoJSON from 'ol/format/GeoJSON.js';
+import Map from 'ol/Map.js';
+import VectorSource from 'ol/source/Vector.js';
+import View from 'ol/View.js';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import OSM from 'ol/source/OSM';
+import Style from 'ol/style/Style';
+import './style.css';
+import Icon from 'ol/style/Icon';
+import Overlay from 'ol/Overlay';
+import { fromLonLat } from 'ol/proj';
+import { Point } from 'ol/geom';
+
+
+
+const vectorSource = new VectorSource({
+  format: new GeoJSON(),
+  url: 'http://localhost:8081/geoserver/yunnan/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=yunnan%3Atest1&maxFeatures=50&outputFormat=application%2Fjson&srsname=EPSG:3857', 
+  
+});
+
+
+const vector = new VectorLayer({
+  source: vectorSource,
+  style: new Style({
+    image: new Icon({
+      src: 'https://docs.maptiler.com/openlayers/default-marker/marker-icon.png',
+    })
+  })
+});
+
+
+const raster = new TileLayer({
+  source: new OSM()
+});
+
+const map = new Map({
+  layers: [raster, vector],
+  target: document.getElementById('map'),
+  view: new View({
+    center: [0,0],
+    zoom: 1,
+  }),
+});
+
+  const container = document.getElementById('popup');
+  const content = document.getElementById('popup-content');
+  const closer = document.getElementById('popup-closer');
+  const img1 = document.getElementById('img1');
+
+  const popup = new Overlay({
+    element: container,
+    autoPan: true,
+    autoPanAnimation: {
+    duration: 250
+  }
+  });
+  map.addOverlay(popup);
+
+  closer.onclick = function () {
+    popup.setPosition(undefined);
+    closer.blur();
+    return false;
+  };
+  
+  map.on('click', function (event) {
+    const features = map.getFeaturesAtPixel (event.pixel);
+
+     if (features.length > 0){
+      const ele = document.getElementById('popuptext');
+      ele.innerHTML = features[0].get("name");  
+      
+      const bilder = features[0].get('bilder');
+      const img1 = document.getElementById('img1');
+      img1.setAttribute("src", bilder);
+      
+      const intro = document.getElementById('intro11');
+      intro.innerHTML = features[0].get("intro");
+
+      const progress = features[0].get('progress');
+      const prog = document.getElementById('pro1');
+      prog.setAttribute("value", progress);
+      
+
+      const coordinates = features[0].getGeometry().getCoordinates();
+      popup.setPosition(coordinates);
+    }
+  });
